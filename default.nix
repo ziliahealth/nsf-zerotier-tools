0473568b4112ea83f7acadad956d392932fffc80
Thierry Proulx <thierry.proulx@ziliahealth.com>
2022-09-08 14:30:16 -0400

    fix bash completion

--- default.nix
+++ default.nix
@@ -38,17 +38,21 @@ buildPythonPackage rec {
   postInstall = ''
     click_exes=( "nsf-zerotier" )
 
+    buildPythonPath "$out"
+
     # Install click application bash completions.
     bash_completion_dir="$out/share/bash-completion/completions"
     mkdir -p "$bash_completion_dir"
     for e in "''${click_exes[@]}"; do
       click_exe_path="$out/bin/$e"
+      patchPythonScript "$click_exe_path"
+      wrapProgram "$click_exe_path" --prefix PATH ':' "$program_PATH"
       click_complete_env_var_name="_$(echo "$e" | tr "[a-z-]" "[A-Z_]")_COMPLETE"
       # TODO: For some reason, running this return a non zero (1) status code. This might
       # be a click library bug. Fill one if so.
-      env "''${click_complete_env_var_name}=source_bash" "$click_exe_path" > "$bash_completion_dir/$e" || true
+      env "''${click_complete_env_var_name}=bash_source" "$click_exe_path" > "$bash_completion_dir/$e" || true
       # Because of the above, check that we got some completion code in the file.
-      cat "$bash_completion_dir/$e" | grep "$e" > /dev/null
+      cat "$bash_completion_dir/$e" | grep "$click_complete_env_var_name" > /dev/null
     done
   '';
 
